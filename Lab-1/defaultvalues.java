6399d0754d1ef705249062409679fb3d68a09eb4
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 12:25:27 +0530

    Create defaultvalues.java

---
+++ Lab-1/defaultvalues.java
@@ -0,0 +1,21 @@
+public class defaultvalues {
+		static int Int;
+		static float Float;
+		static double Double;
+		static boolean Boolean;
+		static long Long;
+		static short Short;
+		static char Char;
+		static byte Byte;
+		public static void main(String[] args){
+			System.out.println("default value of int  is :" + Int);
+			System.out.println("default value of float is : " + Float);
+			System.out.println("default value of double is : " + Double);
+			System.out.println("default value of boolean is : " + Boolean);	
+			System.out.println("default value of long is : " + Long);	
+			System.out.println("default value of short is : " + Short);
+			System.out.println("default value of char is : " + Char);
+			System.out.println("default value of byte is : " + Byte);	
+																
+	}
+}
