f1a3038c676b85e9b888aa0e4ab273b4bd5659eb
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 11:31:35 +0530

    Create Super.java

---
+++ lab-5/Super.java
@@ -0,0 +1,25 @@
+class BankAccount{
+	static double balance;
+	public void setBalance(double balance){
+		this.balance=balance;
+	}
+	public void deposit(double amt){
+		balance=balance+amt;
+	}
+}
+class checking extends BankAccount{
+	void display(double amt){
+		super.deposit(amt);
+		System.out.println("An ammount of "+amt+" has been added");
+		System.out.println("Avl Bal: "+super.balance);
+	}
+}
+public class Super{
+	public static void main(String[] args){
+		BankAccount b=new BankAccount();
+		b.setBalance(2000);
+		b.deposit(300);
+		checking c=new checking();
+		c.display(200);
+	}
+}
