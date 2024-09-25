c48c358e31498d5cc52feab7989291139bc8ff5c
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 11:52:31 +0530

    Create SingleInheritance{.java

---
+++ Lab-4/SingleInheritance{.java
@@ -0,0 +1,18 @@
+class Vehicle{
+	void start(){
+		System.out.println("Vehicle started");
+	}
+}
+class car extends Vehicle{
+	void start(){
+		System.out.println("Car started");
+	}
+}
+public class SingleInheritance{
+	public static void main(String[] args){
+		Vehicle v=new Vehicle();
+		v.start();
+		car c= new car();
+		c.start();
+	}
+}
