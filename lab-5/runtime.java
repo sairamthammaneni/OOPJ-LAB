d3918d561b1c4099ac8ce059e58fd92a036151ef
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 11:49:35 +0530

    Add files via upload

---
+++ lab-5/runtime.java
@@ -0,0 +1,21 @@
+abstract class vehicle{
+	void display(){
+		System.out.println("This is a Vehicle");
+	}
+	abstract void color();
+}
+class Car extends vehicle{	
+	void display(){
+		System.out.println("This is Car");
+	}
+	void color(){
+		System.out.println("Color : Red");
+	}
+}
+public class runtime{
+	public static void main(String[] args){
+		vehicle c=new Car();
+		c.display();
+		c.color();
+	}
+}
