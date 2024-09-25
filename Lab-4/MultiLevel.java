1664010da16bdf8bb5af0bdfd6ca7e868a95ac50
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 11:55:59 +0530

    Create MultiLevel.java

---
+++ Lab-4/MultiLevel.java
@@ -0,0 +1,25 @@
+class Animal{
+	void makesSound(){
+		System.out.println("Animals make sound");
+	}
+}
+class Dog extends Animal{
+	void makesSound(){
+		System.out.println("Dog : Woof!");
+	}	
+}
+class Puppy extends Animal{
+	void makesSound(){
+		System.out.println("Puppy : Yap!");
+	}	
+}
+public class MultiLevel{
+	public static void main(String[] args){
+		Animal a=new Animal();
+		Dog d=new Dog();
+		Puppy p=new Puppy();
+		a.makesSound();
+		d.makesSound();
+		p.makesSound();
+	}
+}
