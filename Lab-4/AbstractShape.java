df96e1c45157f2319063a643b5bc4f53e2b0cf6b
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-25 12:14:22 +0530

    Add files via upload

---
+++ Lab-4/AbstractShape.java
@@ -0,0 +1,32 @@
+abstract class shape{
+	abstract double Area();
+}
+class circle extends shape{
+	double r;
+	circle(double r){
+		this.r=r;
+		System.out.println("Circle");
+	}
+	double Area(){
+		return 3.14*r*r; 
+	}
+}
+class rectangle extends shape{
+	double l,w;
+	rectangle(double l,double w){
+		this.w=w;
+		this.l=l;
+		System.out.println("Rectangle");
+	}
+	double Area(){
+		return l*w;
+	}
+}
+public class AbstractShape{
+	public static void main(String[] args){
+		circle c=new circle(20);
+		System.out.println("Area : "+c.Area());
+		rectangle r=new rectangle(10,15);
+		System.out.println("Area : "+r.Area());
+	}
+}
